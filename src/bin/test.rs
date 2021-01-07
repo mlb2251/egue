1e40e5626bc2bd9f790eceb9b51fb71967c9b5f5
Matt Bowers <mattb6503@gmail.com>
2021-01-07 00:15:00 -0500

    it works!

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -2,6 +2,7 @@ use colored::*;
 //use std::convert::TryInto;
 
 
+
 fn main() {
   // let f: fn(&i32) -> i32 = |x| x+1;
   // let ptr : *const fn(&i32) -> i32 = &f;
@@ -96,40 +97,83 @@ fn run_bottom_up(env:Vec<bottom_up::Val>) {
     vec![
       Prod::new("$0",     Index(0),             &[],                      idx_0),
       Prod::new("$1",     Index(1),             &[],                      idx_1),
+
+      Prod::new("head",    Int.into(),       &[IntList],      head),
+      Prod::new("last",    Int.into(),       &[IntList],      last),
+      Prod::new("take",    Int.into(),       &[Int,IntList],      take),
+      Prod::new("drop",    Int.into(),       &[Int,IntList],      drop),
+      Prod::new("access",    Int.into(),       &[Int,IntList],      access),
+      Prod::new("minimum",    Int.into(),       &[IntList],      minimum),
+      Prod::new("maximum",    Int.into(),       &[IntList],      maximum),
+      Prod::new("reverse",    IntList.into(),       &[IntList],      reverse),
+      Prod::new("sort",    IntList.into(),       &[IntList],      sort),
+      Prod::new("sum",    Int.into(),       &[IntList],      sum),
+
       Prod::new("map",    IntList.into(),       &[IntToInt,IntList],      map),
-      Prod::new("add1",   IntToInt.into(),      &[],                      make_add1),
-      Prod::new("mul2",   IntToInt.into(),      &[],                      make_mul2),
+      Prod::new("filter", IntList.into(),       &[IntToBool,IntList],     filter),
+      Prod::new("count", Int.into(),       &[IntToBool,IntList],     count),
+      Prod::new("zipwith", IntList.into(),       &[IntToIntToInt,IntList,IntList],     zipwith),
+      Prod::new("scanl1", IntList.into(),       &[IntToIntToInt,IntList],     scanl1),
+
+      Prod::new("(+1)",   IntToInt.into(),      &[],                      make_add1),
+      Prod::new("(-1)",   IntToInt.into(),      &[],                      make_sub1),
+      Prod::new("(*2)",   IntToInt.into(),      &[],                      make_mul2),
+      Prod::new("(/2)",   IntToInt.into(),      &[],                      make_div2),
+      Prod::new("(*-1)",   IntToInt.into(),      &[],                      make_negate),
+      Prod::new("(**2)",   IntToInt.into(),      &[],                      make_square),
+      Prod::new("(*3)",   IntToInt.into(),      &[],                      make_mul3),
+      Prod::new("(/3)",   IntToInt.into(),      &[],                      make_div3),
+      Prod::new("(*4)",   IntToInt.into(),      &[],                      make_mul4),
+      Prod::new("(/4)",   IntToInt.into(),      &[],                      make_div4),
+
+
+      Prod::new("(>0)",IntToBool.into(),      &[],                   make_is_gt0),
+      Prod::new("(<0)",IntToBool.into(),      &[],                   make_is_lt0),
+      Prod::new("is_even",IntToBool.into(),      &[],                   make_is_even),
+      Prod::new("is_odd",IntToBool.into(),      &[],                   make_is_odd),
+
+      Prod::new("(+)",IntToIntToInt.into(),      &[],                   make_add),
+      Prod::new("(-)",IntToIntToInt.into(),      &[],                   make_sub),
+      Prod::new("(*)",IntToIntToInt.into(),      &[],                   make_mul),
+      Prod::new("(min)",IntToIntToInt.into(),      &[],                   make_min),
+      Prod::new("(max)",IntToIntToInt.into(),      &[],                   make_max),
     ]
   };
 
   println!("building search state");
-  let mut search_state = SearchState::new(prods, env);
+  let observational_equiv = false;
+  let quiet = false;
+  let mut search_state = SearchState::new(prods, env, observational_equiv, quiet);
 
   println!("running");
-  search_state.run(10);
+  search_state.run(7);
 
 }
 
 pub mod bottom_up {
+  use std::convert::{TryFrom,TryInto};
 
   pub type Result = std::result::Result<Val,Error>;
   pub type DSLFunc = fn(&[&Val], &[Val]) -> Result;
   pub type Id = usize;
   #[derive(Debug, Copy, Clone)]
-  pub enum Type {Int, IntList, IntToInt}
+  pub enum Type {Int, IntList, IntToInt, IntToIntToInt, IntToBool}
   pub enum ReturnType {Concrete(Type), Index(usize)}
   pub enum Error {
-    Runtime,
+    RuntimeError,
     TypeError,
-    IndexError,
-    Other(String),
+    // IndexError,
+    // OutOfBoundsError,
+    // Other(String),
   }
   pub enum Val {
     // an actual value of an intermediate or final result
     // in the DSL
     Int(i32),
     IntList(Vec<i32>),
-    IntToInt(fn(&i32) -> i32)
+    IntToInt(fn(&i32) -> i32),
+    IntToIntToInt(fn(&i32,&i32) -> i32),
+    IntToBool(fn(&i32) -> bool)
   }
   pub enum DisplayExpr {
     // just a class for printing out expressions
@@ -141,9 +185,11 @@ pub mod bottom_up {
     // holds the current state of the search
     pub prods: Vec<Prod>, // production rules
     pub seen: std::collections::HashSet::<Val>, // set of values seen so far
-    pub found_vecs: [Vec<Found>; 3],
+    pub found_vecs: [Vec<Found>; 5],
     pub env: Vec<Val>,
     target_weight: usize,
+    pub observational_equiv: bool,
+    pub quiet: bool,
   }
   pub struct Prod {
     // a production rule
@@ -165,17 +211,19 @@ pub mod bottom_up {
   }
 
   impl SearchState {
-    pub fn new(mut prods: Vec<Prod>, env: Vec<Val>) -> SearchState {
+    pub fn new(mut prods: Vec<Prod>, env: Vec<Val>, observational_equiv: bool, quiet: bool) -> SearchState {
       for (i,prod) in prods.iter_mut().enumerate() {
         prod.id = Some(i);
       }
-      SearchState {prods, env, ..Default::default()}
+      SearchState {prods, env, observational_equiv, quiet, ..Default::default()}
     }
     fn possible_values(&self, arg: Type) -> &Vec<Found> {
         match arg {
           Type::Int => &self.found_vecs[0],
           Type::IntList => &self.found_vecs[1],
           Type::IntToInt => &self.found_vecs[2],
+          Type::IntToIntToInt => &self.found_vecs[3],
+          Type::IntToBool => &self.found_vecs[4],
         }
     }
     pub fn expr_of_found(&self, found: &Found) -> DisplayExpr {
@@ -213,12 +261,15 @@ pub mod bottom_up {
       // run the DSLFunc
       let res = (prod.func)(&arg_vals[..], &self.env[..]);
       let val = match res {
-        Err(Error::TypeError) => panic!("we shouldnt never encouter type errors"),
-        Err(_) => panic!("havent handled whatever this is"),
+        Err(Error::RuntimeError) => {return None} // program crashes when you try to run it
+        Err(Error::TypeError) => panic!("we should never encouter type errors"),
+        //Err(_) => panic!("havent handled whatever this is"),
         Ok(val) => val,
       };
       //observational equivalence
-      if self.seen.contains(&val) { return None }
+      if self.observational_equiv{
+        if self.seen.contains(&val) { return None }
+      }
 
       let mut arg_ids = [None;3];
       let id_vec: Vec<Id> = args.iter().map(|found| found.id).map(|x|x.unwrap()).collect();
@@ -309,14 +360,20 @@ pub mod bottom_up {
 
       // update with newly found values
       for mut found in to_add.into_iter(){
-        println!("{:?} -> {:?} ", self.expr_of_found(&found), found.val);
+        if !self.quiet {
+          println!("{:?} -> {:?} ", self.expr_of_found(&found), found.val);
+        }
         let v = match found.val.ty() {
           Type::Int => &mut self.found_vecs[0],
           Type::IntList => &mut self.found_vecs[1],
           Type::IntToInt => &mut self.found_vecs[2],
+          Type::IntToIntToInt => &mut self.found_vecs[3],
+          Type::IntToBool => &mut self.found_vecs[4],
         };
         found.id = Some(v.len()); // set the id of Found
-        self.seen.insert(found.val.clone()); // mark Found.val as seen
+        if self.observational_equiv{
+          self.seen.insert(found.val.clone()); // mark Found.val as seen
+        }
         v.push(found);
       }
     }
@@ -339,41 +396,204 @@ pub mod bottom_up {
     use super::*;
     use super::Val::*;
     use super::Error::*;
-    pub fn map(args: &[&Val], _env: &[Val]) -> Result {
-      if let [IntToInt(f),IntList(xs)] = args {
-        Ok(IntList(xs.iter().map(|x|f(x)).collect()))
-      } else { Err(TypeError) }
-    }
-    pub fn make_add1(args: &[&Val], _env: &[Val]) -> Result{
-      if let [] = args {
-        Ok(IntToInt(|x|x+1))
-      } else { Err(TypeError) }
-    }
     pub fn idx_0(args: &[&Val], env: &[Val]) -> Result{
       if let [] = args {
         // explicit clone is needed here. In stuff like map() we construct
         // a new value so you get a Val naturally, but here we just lookup
         // a &Val so we need to clone it into a fresh Val
-        env.get(0).ok_or(IndexError).map(|v|v.clone())
+        env.get(0).ok_or(RuntimeError).map(|v|v.clone())
       } else { Err(TypeError) }
     }
     pub fn idx_1(args: &[&Val], env: &[Val]) -> Result{
       if let [] = args {
-        env.get(1).ok_or(IndexError).map(|v|v.clone())
+        env.get(1).ok_or(RuntimeError).map(|v|v.clone())
       } else { Err(TypeError) }
     }
-    // pub fn make_const(v: Val) -> DSLFunc{
-    //   let v = v.clone();
-    //   let f = |args: &[Val]| -> Result{
-    //     if let [] = args {
-    //       Ok(v)
-    //     } else { Err(TypeError) }
-    //   };
-    //   f
-    // }
+
+    pub fn head(args: &[&Val], _env: &[Val]) -> Result {
+      if let [IntList(xs)] = args {
+        xs.first().ok_or(RuntimeError).map(|v|Int(v.clone()))
+      } else { Err(TypeError) }
+    }
+    pub fn last(args: &[&Val], _env: &[Val]) -> Result {
+      if let [IntList(xs)] = args {
+        xs.last().ok_or(RuntimeError).map(|v|Int(v.clone()))
+      } else { Err(TypeError) }
+    }
+    pub fn take(args: &[&Val], _env: &[Val]) -> Result {
+      if let [Int(n),IntList(xs)] = args {
+        let n = usize::try_from(*n).map_err(|_| RuntimeError)?; // turn n < 0 into runtime error
+        // per deepcoder, we are okay with the normal take() behavior
+        // where n > len() just returns the full original vector
+        Ok(IntList(xs.iter().take(n).copied().collect()))
+      } else { Err(TypeError) }
+    }
+    pub fn drop(args: &[&Val], _env: &[Val]) -> Result {
+      if let [Int(n),IntList(xs)] = args {
+        let n = usize::try_from(*n).map_err(|_| RuntimeError)?; // turn n < 0 into runtime error
+        // per deepcoder if n > len() we return an empty vector
+        Ok(IntList(xs.iter().skip(n).copied().collect()))
+      } else { Err(TypeError) }
+    }
+    pub fn access(args: &[&Val], _env: &[Val]) -> Result {
+      if let [Int(n),IntList(xs)] = args {
+        let n = usize::try_from(*n).map_err(|_| RuntimeError)?; // turn n < 0 into runtime error
+        xs.get(n).ok_or(RuntimeError).map(|v|Int(v.clone()))
+      } else { Err(TypeError) }
+    }
+    pub fn minimum(args: &[&Val], _env: &[Val]) -> Result {
+      if let [IntList(xs)] = args {
+        xs.iter().min().ok_or(RuntimeError).map(|v|Int(v.clone()))
+      } else { Err(TypeError) }
+    }
+    pub fn maximum(args: &[&Val], _env: &[Val]) -> Result {
+      if let [IntList(xs)] = args {
+        xs.iter().max().ok_or(RuntimeError).map(|v|Int(v.clone()))
+      } else { Err(TypeError) }
+    }
+    pub fn reverse(args: &[&Val], _env: &[Val]) -> Result {
+      if let [IntList(xs)] = args {
+        Ok(IntList(xs.iter().rev().copied().collect()))
+      } else { Err(TypeError) }
+    }
+    pub fn sort(args: &[&Val], _env: &[Val]) -> Result {
+      if let [IntList(xs)] = args {
+        let mut res = xs.clone();
+        res.sort();
+        Ok(IntList(res))
+      } else { Err(TypeError) }
+    }
+    pub fn sum(args: &[&Val], _env: &[Val]) -> Result {
+      if let [IntList(xs)] = args {
+        Ok(Int(xs.iter().sum::<i32>()))
+      } else { Err(TypeError) }
+    }
+    
+    pub fn map(args: &[&Val], _env: &[Val]) -> Result {
+      if let [IntToInt(f),IntList(xs)] = args {
+        Ok(IntList(xs.iter().map(|x|f(x)).collect()))
+      } else { Err(TypeError) }
+    }
+    pub fn filter(args: &[&Val], _env: &[Val]) -> Result {
+      if let [IntToBool(f),IntList(xs)] = args {
+        Ok(IntList(xs.iter().filter(|x|f(x)).copied().collect()))
+      } else { Err(TypeError) }
+    }
+    pub fn count(args: &[&Val], _env: &[Val]) -> Result {
+      if let [IntToBool(f),IntList(xs)] = args {
+        // panic if this i32 -> usize conversion fails bc it should never...
+        Ok(Int(xs.iter().filter(|x|f(x)).count().try_into().unwrap()))
+      } else { Err(TypeError) }
+    }
+    pub fn zipwith(args: &[&Val], _env: &[Val]) -> Result {
+      if let [IntToIntToInt(f),IntList(xs),IntList(ys)] = args {
+        Ok(IntList(xs.iter().zip(ys.iter()).map(|(x,y)|f(x,y)).collect()))
+      } else { Err(TypeError) }
+    }
+    pub fn scanl1(args: &[&Val], _env: &[Val]) -> Result {
+      if let [IntToIntToInt(f),IntList(xs)] = args {
+        // if input is empty list, return empty list
+        if xs.len() == 0 {return Ok(IntList(xs.clone()))}
+        let init = xs.first().unwrap().clone(); //ok to unwrap bc already checked for empty list
+        Ok(IntList(xs.iter().skip(1).scan(init,|x,y|Some(f(x,y))).collect()))
+      } else { Err(TypeError) }
+    }
+
+    pub fn make_add1(args: &[&Val], _env: &[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToInt(|x|x+1))
+      } else { Err(TypeError) }
+    }
+    pub fn make_sub1(args: &[&Val], _env: &[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToInt(|x|x-1))
+      } else { Err(TypeError) }
+    }
     pub fn make_mul2(args: &[&Val], _env:&[Val]) -> Result{
       if let [] = args {
         Ok(IntToInt(|x|x*2))
+      } else { Err(TypeError) }
+    }
+    pub fn make_div2(args: &[&Val], _env:&[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToInt(|x|x/2)) // integer division
+      } else { Err(TypeError) }
+    }
+    pub fn make_negate(args: &[&Val], _env:&[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToInt(|x|x*-1))
+      } else { Err(TypeError) }
+    }
+    pub fn make_square(args: &[&Val], _env:&[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToInt(|x|x*x))
+      } else { Err(TypeError) }
+    }
+    pub fn make_mul3(args: &[&Val], _env:&[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToInt(|x|x*3))
+      } else { Err(TypeError) }
+    }
+    pub fn make_div3(args: &[&Val], _env:&[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToInt(|x|x/3))
+      } else { Err(TypeError) }
+    }
+    pub fn make_mul4(args: &[&Val], _env:&[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToInt(|x|x*4))
+      } else { Err(TypeError) }
+    }
+    pub fn make_div4(args: &[&Val], _env:&[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToInt(|x|x/4))
+      } else { Err(TypeError) }
+    }
+
+    pub fn make_is_gt0(args: &[&Val], _env: &[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToBool(|x|*x>0))
+      } else { Err(TypeError) }
+    }
+    pub fn make_is_lt0(args: &[&Val], _env: &[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToBool(|x|*x<0))
+      } else { Err(TypeError) }
+    }
+    pub fn make_is_even(args: &[&Val], _env: &[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToBool(|x|x%2==0))
+      } else { Err(TypeError) }
+    }
+    pub fn make_is_odd(args: &[&Val], _env: &[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToBool(|x|x%2==1))
+      } else { Err(TypeError) }
+    }
+
+    pub fn make_add(args: &[&Val], _env: &[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToIntToInt(|x,y|x+y))
+      } else { Err(TypeError) }
+    }
+    pub fn make_sub(args: &[&Val], _env: &[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToIntToInt(|x,y|x-y))
+      } else { Err(TypeError) }
+    }
+    pub fn make_mul(args: &[&Val], _env: &[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToIntToInt(|x,y|x*y))
+      } else { Err(TypeError) }
+    }
+    pub fn make_min(args: &[&Val], _env: &[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToIntToInt(|&x,&y|std::cmp::min(x,y)))
+      } else { Err(TypeError) }
+    }
+    pub fn make_max(args: &[&Val], _env: &[Val]) -> Result{
+      if let [] = args {
+        Ok(IntToIntToInt(|&x,&y|std::cmp::max(x,y)))
       } else { Err(TypeError) }
     }
   }
@@ -388,6 +608,8 @@ pub mod bottom_up {
         Val::Int(_) => Type::Int,
         Val::IntList(_) => Type::IntList,
         Val::IntToInt(_) => Type::IntToInt,
+        Val::IntToIntToInt(_) => Type::IntToIntToInt,
+        Val::IntToBool(_) => Type::IntToBool,
       }
     }
   }
@@ -398,6 +620,8 @@ pub mod bottom_up {
         Val::Int(v) => Val::Int(v.clone()),
         Val::IntList(v) => Val::IntList(v.clone()),
         Val::IntToInt(f) => Val::IntToInt(f.clone()),
+        Val::IntToIntToInt(f) => Val::IntToIntToInt(f.clone()),
+        Val::IntToBool(f) => Val::IntToBool(f.clone()),
       }
     }
   }
@@ -411,6 +635,14 @@ pub mod bottom_up {
         Val::IntList(v) => v.hash(state),
         Val::IntToInt(f) => {
           let ptr : *const fn(&i32) -> i32 = f;
+          ptr.hash(state)
+        }
+        Val::IntToIntToInt(f) => {
+          let ptr : *const fn(&i32,&i32) -> i32 = f;
+          ptr.hash(state)
+        }
+        Val::IntToBool(f) => {
+          let ptr : *const fn(&i32) -> bool = f;
           ptr.hash(state)
         }
       };
@@ -428,6 +660,16 @@ pub mod bottom_up {
           let ptr2 : *const fn(&i32) -> i32 = o;
           ptr == ptr2
         }
+        (Val::IntToIntToInt(f),Val::IntToIntToInt(o)) => {
+          let ptr : *const fn(&i32,&i32) -> i32 = f;
+          let ptr2 : *const fn(&i32,&i32) -> i32 = o;
+          ptr == ptr2
+        }
+        (Val::IntToBool(f),Val::IntToBool(o)) => {
+          let ptr : *const fn(&i32) -> bool = f;
+          let ptr2 : *const fn(&i32) -> bool = o;
+          ptr == ptr2
+        }
         _ => false,
       }
     }
@@ -440,6 +682,8 @@ pub mod bottom_up {
         Val::Int(x)  => write!(f,"Int({})",x),
         Val::IntList(x)  => write!(f,"IntList({:?})",x),
         Val::IntToInt(_)  => write!(f,"IntToInt(cant_display)"),
+        Val::IntToIntToInt(_)  => write!(f,"IntToIntToInt(cant_display)"),
+        Val::IntToBool(_)  => write!(f,"IntToBool(cant_display)"),
       }
     }
   }
