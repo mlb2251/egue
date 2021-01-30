350899189baed0109611831682d93ab983b04d11
Matt Bowers <mattb6503@gmail.com>
2021-01-30 12:21:45 -0500

    added ex_bench

---
+++ examples/ex_bench.rs
@@ -0,0 +1,20 @@
+use egue::bottom_up::{
+    Val,
+    SearchState,
+    deepcoder,
+};
+use std::time::Instant;
+
+fn main() {
+  let env = vec![Val::Int(3),Val::IntList(vec![1,2,3,4,5])];
+  let prods = deepcoder::get_prods(env.len());
+  let observational_equiv = true;
+  let quiet = true;
+  let silent = false;
+  let target = None;
+  let mut search_state = SearchState::new(prods, env, observational_equiv, quiet, silent, target);
+  let tstart = Instant::now();
+  search_state.run(12);
+  let elapsed = tstart.elapsed().as_secs_f32();
+  println!("{:.2}", elapsed);
+}
